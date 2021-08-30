930ba0c85d2f19f2b131eeb1010c513884192786
Josh <dandobusiness@gmail.com>
2021-08-30 15:23:57 +0100

    feat: allow https connection to self signed certificate
    by default android uses https, you can overide this using the network_security_config however the ip address must be known before hand
    For https calls to work a valid cert must be used but philip hues do not provide a valid certificate for their api endpoint thus results in a cert error.
    To bypass this we create our own SSL which is made by using a default trust manager containing our generated cert. We then overide the default https url connection to use said SSL and our own hostname verifier

--- app/src/main/java/com/example/huedisco/MainActivity.kt
+++ app/src/main/java/com/example/huedisco/MainActivity.kt
@@ -3,8 +3,30 @@ package com.example.huedisco
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.example.huedisco.databinding.ActivityMainBinding
 import java.util.regex.Pattern
+
+import java.security.KeyStore
+
+import java.io.IOException
+import java.security.cert.Certificate
+
+import java.security.cert.CertificateFactory
+
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
+import javax.net.ssl.HttpsURLConnection
+
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLSession
+import com.android.volley.toolbox.HurlStack
+import java.net.URL
+
+import java.net.HttpURLConnection
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -28,11 +50,92 @@ class MainActivity : AppCompatActivity() {
         binding.connectToBridge.setOnClickListener{connectToBridge()}
     }
 
+    private fun getHostnameVerifier(): HostnameVerifier {
+        return object : HostnameVerifier {
+            override fun verify(hostname: String, session: SSLSession): Boolean {
+                val hv = HttpsURLConnection.getDefaultHostnameVerifier()
+                Log.d("hv", hv.toString())
+                val ip = binding.editIpAddress.text.toString()
+                Log.d("ip to verify: ", ip)
+                Log.d("session host: ", session.peerHost)
+//                return hv.verify(ip, session)
+                return true // insecure as allows any SSL connection with cert
+            }
+        }
+    }
+
+    var hurlStack: HurlStack = object : HurlStack() {
+        @Throws(IOException::class)
+        override fun createConnection(url: URL): HttpURLConnection {
+            val httpsURLConnection = super.createConnection(url) as HttpsURLConnection
+            try {
+                httpsURLConnection.sslSocketFactory = getSSLSocketFactory()
+                httpsURLConnection.hostnameVerifier = getHostnameVerifier()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return httpsURLConnection
+        }
+    }
+
+    private fun getSSLSocketFactory(): SSLSocketFactory {
+
+            val cf = CertificateFactory.getInstance("X.509");
+            val id = this.resources.getIdentifier("my_cert", "raw", this.packageName)
+
+            val caInput = resources.openRawResource(id);
+            var ca: Certificate;
+
+            caInput.use { caInput ->
+                ca = cf.generateCertificate(caInput) as X509Certificate;
+                Log.e("CERT", "ca=" + (ca as X509Certificate).getSubjectDN());
+            }
+
+
+            val keyStoreType = KeyStore.getDefaultType();
+            val keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", ca);
+
+
+            var tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            var tmf: TrustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(keyStore);
+
+            var context: SSLContext = SSLContext.getInstance("TLS");
+            context.init(null, tmf.getTrustManagers(), null);
+
+            return context.socketFactory;
+
+    }
+
+
     private fun connectToBridge() {
 
 //        validate IP: Patterns.IP_ADDRESS.matcher(url).matches();
         val ip = binding.editIpAddress.text
         Log.d("Ip address: ", ip.toString())
+
+//        verify our connection to the bridge
+
+        val url = "https://$ip/api/12345/config"
+
+//        val url = "https://pokeapi.co/api/v2/ability/7/"
+        val queue = Volley.newRequestQueue(this, hurlStack)
+        Log.d("queue",  "created new request queue")
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                Log.d("Response: ", response.toString())
+            },
+            Response.ErrorListener { error ->
+                // TODO: Handle error
+                Log.d("Error: ", error.toString())
+            }
+        )
+
+        queue.add(jsonObjectRequest)
+
 //        TODO: connect to bridge
 //        TODO: if successful then go to next page
     }
